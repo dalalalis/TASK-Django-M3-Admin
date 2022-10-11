d6ab34629df9cdebd4d1571057acb3a231bd152c
Dalal Alsabah <dasabah@gmail.com>
2022-10-11 20:07:37 +0300

    first part done

--- pokemon/models.py
+++ pokemon/models.py
@@ -1,3 +1,27 @@
+from email.policy import default
+from random import choices
+from tkinter.tix import AUTO
+from turtle import update
 from django.db import models
+from django.forms import CharField, DateTimeField, IntegerField
 
 # Create your models here.
+class Pokemon( models.Model):
+    class PokemonType (models.TextChoices):
+        WATER = 'WA'
+        GRASS = 'GR'
+        GHOST = 'GH'
+        STEEL = 'ST'
+        FAIRY = 'FA'
+
+    name=models.CharField(max_length=30)
+    type=models.CharField(max_length=5,choices= PokemonType.choices)
+    hp=models.PositiveIntegerField
+    active=models.BooleanField(default=True)
+    name_fr=models.CharField(max_length=30, default="")
+    name_ar=models.CharField(max_length=30, default="")
+    name_jp=models.CharField(max_length=30, default="")
+    created_at=models.DateTimeField(auto_now_add=True)
+    modified_at=models.DateTimeField(auto_now=True)
+
+
